1712f6636990bad650fd6914cfae3df460d8dce7
Brian Modzelewski <bmmodz456@gmail.com>
2024-02-21 16:31:19 -0500

    Module 6

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -4,7 +4,7 @@ const controller= require('../controllers/travel');
 
 
 /* GET travel page. */
-console.log('Inside app_server, routes, travel.js.');
-router.get('/', controller.travel);
+
+router.get('/', controller.travelList);
 
 module.exports = router;
